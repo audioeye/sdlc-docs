1bb974f9c2ae7b6d28ff98bc170ee3e6e254b117
rhigdon@audioeye.com <rhigdon@audioeye.com>
2023-09-19 16:17:39 -0700

    Fixing deploy command

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/docs/src/.vuepress/dist.git master:gh-pages
+git push -f git@github.com:animated-disco-298w81w/docs/src/.vuepress/dist.git main:gh-pages
 
 cd -
 
