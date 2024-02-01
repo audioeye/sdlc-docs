218d06da621f9cc2e29d3103b186de6ba7a7c3fc
rhigdon <rhigdon@audioeye.com>
2024-01-31 17:41:17 -0800

    DX-179 | @rhigdon | Adding Skip to Main link

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,7 +1,14 @@
 import { defineClientConfig } from '@vuepress/client'
+import Layout from './layouts/Layout.vue'
+// import SkipToMain from './components/SkipToMain.vue'
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {},
   setup() {},
-  rootComponents: [],
+  // rootComponents: [
+  //   SkipToMain
+  // ],
+  layouts: {
+    Layout
+  }
 })
---
+++ docs/.vuepress/layouts/Layout.vue
@@ -0,0 +1,31 @@
+<script setup>
+import ParentLayout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
+</script>
+
+<template>
+  <ParentLayout>
+    <template #navbar-before>
+        <a href="#main" class="skip-to-main">Skip to main content</a>
+    </template>
+  </ParentLayout>
+</template>
+
+<style lang="css">
+.skip-to-main {
+  opacity: 0;
+  z-index: -999;
+  position: absolute;
+}
+.skip-to-main:focus {
+  background-color: #fff;
+  top: 10px;
+  right: 15px;
+  padding: 8px 16px;
+  border-radius: 8px;
+  -webkit-transition: all 150ms;
+  transition: all 150ms;
+  opacity: 1;
+  z-index: 100;
+}
+</style>
+
