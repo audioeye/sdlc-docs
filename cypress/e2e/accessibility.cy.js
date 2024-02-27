df8d7301a0dd90cc0cf38c958931c08ac49cba7d
Jonathan Wilson <jonathan.wilson@audioeye.com>
2024-02-26 16:27:21 -0800

    NOJIRA | @jwlsinx | Migrate from vuepress to docusaurus

---
+++ cypress/e2e/accessibility.cy.js
@@ -0,0 +1,23 @@
+import promisify from 'cypress-promise';
+
+describe('accessibility tests', () => {
+  it('test the different docs for accessibility issues', async () => {
+    cy.visit('http://localhost:3000');
+
+    // For full page scan that uses the root element, we need to "get" something first otherwise the page won't be settled
+    cy.get('[id="__docusaurus_skipToContent_fallback"]').should('exist');
+
+    const htmlRootElement = await promisify(cy.get('html:root'));
+    await expect(htmlRootElement).to.onlyHaveTheseAccessibilityIssues(
+      'Html_Name_Redundant',
+      'BadTag_Emphasis_Detect',
+      'BadTag_Presentational_Detect',
+      'Svg_Name_Missing',
+      'Svg_Name_Missing',
+      'Svg_Name_Missing',
+      'Link_Name_Redundant',
+      'Html_Possible_Heading',
+      'Html_Possible_Heading',
+    );
+  });
+});
